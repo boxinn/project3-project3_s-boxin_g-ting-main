591e041cf25dc5a64bf9de6fc5a4dc7d5513d983
boxin <92691567+boxinn@users.noreply.github.com>
2022-03-06 13:55:33 -0600

    Changes

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project 3: Kernel SVM
+
+View 03kernelsvm.html for detailed instructions on the assignment.
+
---
+++ recoverBias.py
@@ -0,0 +1,41 @@
+"""
+INPUT:	
+K : nxn kernel matrix
+yTr : nx1 input labels
+alphas  : nx1 vector or alpha values
+C : regularization constant
+
+Output:
+bias : the scalar hyperplane bias of the kernel SVM specified by alphas
+
+Solves for the hyperplane bias term, which is uniquely specified by the support vectors with alpha values
+0<alpha<C
+"""
+
+import numpy as np
+
+def recoverBias(K,yTr,alphas,C):
+    bias = 0
+    
+    # YOUR CODE HERE
+    d,n=np.shape(yTr)
+    al=alphas
+    for i in range(0,n):
+        if al[i]<0 and al[i]>C:
+            al[i]=0
+
+    a=n-np.count_nonzero(al)
+
+    AL=(al>0).flatten
+    sum1=0
+    for i in range(0,n):
+        sum2=yTr[i]-sum1
+        for j in range(0,n):
+            sum1=al[i]*yTr[i]*K[i,j]
+
+    bias = sum2/a
+
+
+    
+    return bias 
+    
